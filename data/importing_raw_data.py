599a1bc459b471013dae37e3907deb9edef1ae2c
Gokul Kannan <156730590+gkannan-codes@users.noreply.github.com>
2025-09-20 17:48:26 -0400

    Add files via upload

---
+++ data/importing_raw_data.py
@@ -0,0 +1,10 @@
+# importing pandas in order to read a CSV file from a URL
+import pandas as pd
+
+# URL of the CSV file
+url = ("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?"
+       "query=select+*+from+pscomppars&format=csv")
+
+df = pd.read_csv(url)                                      # loads directly from the web into a dataframe
+df.to_csv("nasa_exoplanets_raw_data.csv", index=False)     # saves the dataframe to a local CSV file
+print(df.head())                                           # prints the first few rows of the dataframe
